f89595e33886d6764a68e00d9e376df1f8fa159d
Marat <xakim@inbox.lv>
2022-08-01 00:02:55 +0300

    adding tests

---
+++ src/test/java/com/customer/FeatureAUtilsTest.java
@@ -0,0 +1,27 @@
+package com.customer;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FeatureAUtilsTest {
+
+    @Test
+    void verifyTransformToLower() {
+        String text = FeatureAUtils.transformToLower("TEXT");
+        Assert.assertEquals("text", text);
+    }
+
+    @Test
+    void verifyTransformToLowerNumbers() {
+        String text = FeatureAUtils.transformToLower("1234321");
+        Assert.assertEquals("1234321", text);
+    }
+
+    @Test
+    void verifyTransformToLowerSpecialChars() {
+        String text = FeatureAUtils.transformToLower("1234321");
+        Assert.assertEquals("1234321", text);
+    }
+}
